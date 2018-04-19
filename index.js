c76fbc15b23fab713490abc89d0cc09f1aaaaf11
Sidharth <sidhu1235@gmail.com>
2018-04-19 08:29:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function progressiveAddition(numArray){
 	if (numArray.length === 1) {
 		return numArray[0];
 	} else {
-		return numArray.pop() + progressiveAddition(my_array);
+		return numArray.pop() + progressiveAddition(numArray);
 	}
 }
